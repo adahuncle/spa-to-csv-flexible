995d333e406ae64d40ba1d485af328703eaf435b
adahuncle <159620579+adahuncle@users.noreply.github.com>
2024-09-24 17:11:21 -0400

    Update convert_one_spa_file_to_csv.py
    Allowed for variable data frame size. My IR spectra was a different size than the original and this way it is more flexible for different instruments.

--- convert_one_spa_file_to_csv.py
+++ convert_one_spa_file_to_csv.py
@@ -2,14 +2,29 @@ import spectrochempy as scp
 import pandas as pd
 import numpy as np
 
-spa_file_path = r'/Users/danielsinausia/Downloads/DS_00145_01.SPA'
-csv_file_path = r'/Users/danielsinausia/Downloads/DS_00145_01.csv'
+# Define file paths
+spa_file_path = r'file.spa' # In
+csv_file_path = r'file.csv' # Out
 
+# Read the SPA file
 dataset = scp.read(spa_file_path)
 data = dataset.data.T
+
+# Create a DataFrame from the dataset
 df = pd.DataFrame(data, columns=dataset.x_labels)
 
+# Define the initial wavenumbers array (6950 values)
 wavenumbers = np.linspace(4000.1885, 649.9040, 6950)
+
+# Check if wavenumbers length is shorter than DataFrame length and pad if necessary (This was necessary for the SPA files from the IR in my lab)
+if len(wavenumbers) < len(df):
+    wavenumbers = np.pad(wavenumbers, (0, len(df) - len(wavenumbers)), 'edge')
+
+# Insert the wavenumbers as a new column into the DataFrame
 df.insert(0, 'Wavenumber', wavenumbers)
 
+# Save the DataFrame to a CSV file
 df.to_csv(csv_file_path, index=False, header=False)
+
+#Final Confirmation
+print(f"CSV file saved successfully to {csv_file_path}")
